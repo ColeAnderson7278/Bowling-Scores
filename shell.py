b82e3945e6a0ac4ee1bec804da74ca01a231dbce
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2018-08-01 09:30:18 -0500

    The start of my bowling score application.

---
+++ shell.py
@@ -0,0 +1,15 @@
+def intro():
+    name = input('Welcome to The Bowling Alley!\nWhat is your name? ')
+    return name
+
+
+def get_score():
+    print
+
+
+def main():
+    name = intro()
+
+
+if __name__ == '__main__':
+    main()
