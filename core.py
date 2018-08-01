19c0239d5290c7542f0853f501796b3fb0a99f0a
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2018-08-01 10:12:06 -0500

    Needs a lot of improvment but I have my bowling application adding scores at a basic level.

--- core.py
+++ core.py
@@ -1,1 +1,6 @@
-
+def score_adding(rounds, score, game):
+    if rounds == 1:
+        return score
+    else:
+        rounds = int(rounds) - 2
+        return (game[rounds]) + score
--- shell.py
+++ shell.py
@@ -1,3 +1,6 @@
+import core
+
+
 def intro():
     name = input('Welcome to The Bowling Alley!\nWhat is your name? ')
     return name
@@ -8,27 +11,37 @@ def get_score():
     score = 0
     while True:
         first_ball = input('\nFirst Ball: ')
-        if first_ball == 10:
+        if int(first_ball) == 10:
             return 10
-        elif 0 <= first_ball < 10:
+        elif 0 <= int(first_ball) < 10:
             score += int(first_ball)
+            while True:
+                second_ball = input('\nSecond Ball: ')
+                if (score + int(second_ball)) > 10:
+                    print('\nPlease enter the correct score.')
+                elif 0 <= int(second_ball) <= 10:
+                    score += int(second_ball)
+                    return score
+                else:
+                    print('Please enter the correct score.')
         else:
-            print('Please enter the correct score.')
-    if score != 10:
-        while True:
-            second_ball = input('\nSecond Ball: ')
-            if (scores + second_ball) > 10:
-                print('Please enter the correct score.')
-            elif 0 <= second_ball <= 10:
-                score += int(second_ball)
-            else:
-                print('Please enter the correct score.')
+            print('\nPlease enter the correct score.')
     return score
 
 
+def show_score(game):
+    print(game)
+
+
 def main():
+    game = []
     name = intro()
-    score = get_score()
+    rounds = 1
+    while rounds <= 10:
+        score = get_score()
+        game.append(core.score_adding(rounds, score, game))
+        show_score(game)
+        rounds += 1
 
 
 if __name__ == '__main__':
